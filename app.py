5c6bd297deeb33e3eacd89b2d474222e0bcd21a5
adamin <minge.adam@gmail.com>
2019-08-10 19:27:04 +0200

    changed routing, connect app with errors dict

--- app.py
+++ app.py
@@ -5,12 +5,14 @@ from flask_socketio import SocketIO
 from flask_jwt_extended import JWTManager
 from flask_login import LoginManager
 from flask_restful import Api
+from errors import errors
 
 
 app = Flask(__name__)
-app.config.from_object(os.environ.get('FLASK_ENV') or 'config.DevelopementConfig')
+app.config.from_object(os.environ.get('FLASK_ENV') or 'config.ProductionConfig')
 
-api = Api(app)
+
+api = Api(app, errors=errors)
 db = SQLAlchemy(app)
 socketio = SocketIO(app)
 jwt = JWTManager(app)
@@ -19,28 +21,61 @@ login_manager = LoginManager(app)
 
 def add_auth_resources():
     from endpoints import auth
-    api.add_resource(auth.UserRegistration, '/registration')
-    api.add_resource(auth.UserLogin, '/login')
-    api.add_resource(auth.TokenRefresh, '/token/refresh')
-    api.add_resource(auth.UserTokens, '/tokens')
-    api.add_resource(auth.UserLogoutRefresh, '/logout/refresh')
-    api.add_resource(auth.UserLogoutAccess, '/logout/access')
+    api.add_resource(auth.UserRegistration, '/registration',
+                     endpoint='registration')
+    api.add_resource(auth.UserLogin, '/login',
+                     endpoint='login')
+    api.add_resource(auth.UserFreshLogin, '/fresh-login',
+                     endpoint='fresh-login')
+    api.add_resource(auth.TokenRefresh, '/token/refresh',
+                     endpoint='token_refresh')
+    api.add_resource(auth.UserLogoutRefresh, '/logout/refresh',
+                     endpoint='user_logout_refresh')
+    api.add_resource(auth.UserLogoutAccess, '/logout/access',
+                     endpoint='user_logout_access')
 
 
 def add_user_resources():
     from endpoints import user
-    api.add_resource(user.User, '/user/<string:username>')
-    api.add_resource(user.UsersCollection, '/users')
+    api.add_resource(user.UserByUsername, '/user/<string:username>',
+                     endpoint='user_by_username')
+    api.add_resource(user.UserTokens, '/user/<string:username>/tokens',
+                     endpoint='user_tokens')
+    api.add_resource(user.UserTokenById, '/user/<string:username>/token/<int:token_id>',
+                     endpoint='user_token_by_id')
+    api.add_resource(user.UsersCollection, '/users',
+                     endpoint='users_collection')
+    api.add_resource(user.UserProjects, '/user/<string:username>/projects',
+                     endpoint='user_projects')
+
+
+def add_project_resources():
+    from endpoints import project
+    api.add_resource(project.ProjectById, '/project/<int:project_id>',
+                     endpoint='project_by_id')
+    api.add_resource(project.ProjectsCollection, '/projects',
+                     endpoint='projects_collection')
+    api.add_resource(project.ProjectUsers, '/project/<int:project_id>/users',
+                     endpoint='project_users')
+    api.add_resource(project.ProjectDiagrams, '/project/<int:project_id>/diagrams',
+                     endpoint='project_diagrams')
+    api.add_resource(project.ProjectDiagramById, '/project/<int:project_id>/diagrams/<int:diagram_id>',
+                     endpoint='project_diagram_by_id')
+    api.add_resource(project.ProjectPermissions, '/project/<int:project_id>/permissions',
+                     endpoint='project_permissions')
+    api.add_resource(project.ProjectPermissionById, '/project/<int:project_id>/permission/<int:project_permission_id>',
+                     endpoint='project_permission_by_id')
 
 
 if __name__ == "__main__":
-    from utils import is_token_revoked
+    from utils.model_queries import token_blacklist
 
     @jwt.token_in_blacklist_loader
     def check_if_token_revoked(decoded_token):
-        return is_token_revoked(decoded_token)
+        return token_blacklist.is_token_revoked(decoded_token)
 
     add_auth_resources()
     add_user_resources()
+    add_project_resources()
 
     socketio.run(app)
