a154458f4fb08fbeaab685508811ae7558bf5beb
adamin <minge.adam@gmail.com>
2019-08-11 12:29:20 +0200

    added possibility to create diagram into project

--- endpoints/project/projects_diagrams.py
+++ endpoints/project/projects_diagrams.py
@@ -1,9 +1,13 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from models import ProjectPermissionModel
 from flask_jwt_extended import jwt_required
 from utils.decorators import pagination, auth
-from utils.model_queries import project
+from utils.model_queries import project, diagram
 from utils.model_fields import diagram_fields
+
+
+add_diagram_perser = reqparse.RequestParser()
+add_diagram_perser.add_argument('name', type=str, required=True)
 
 
 class ProjectDiagrams(Resource):
@@ -13,3 +17,13 @@ class ProjectDiagrams(Resource):
     @pagination.paginate()
     def get(self, project_id):
         return project.get_project_diagrams(project_id)
+
+    @jwt_required
+    @auth.check_user_project_permission(ProjectPermissionModel.ProjectPermissionTypes.Editor)
+    @pagination.marshal_with(diagram_fields)
+    @pagination.paginate()
+    def post(self, project_id):
+        data = add_diagram_perser.parse_args()
+        return diagram.create_diagram(
+            diagram_name=data['name'],
+            project_id=project_id)
