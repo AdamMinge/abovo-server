b1a49d26dcb217df7be30c26946bb02804a2caf2
adamin <minge.adam@gmail.com>
2019-08-11 11:50:27 +0200

    implemented project_users endpoint

--- endpoints/project/project_users.py
+++ endpoints/project/project_users.py
@@ -1,5 +1,15 @@
 from flask_restful import Resource
+from models import ProjectPermissionModel
+from flask_jwt_extended import jwt_required
+from utils.decorators import pagination, auth
+from utils.model_queries import project
+from utils.model_fields import user_fields
 
 
 class ProjectUsers(Resource):
-    pass
+    @jwt_required
+    @auth.check_user_project_permission(ProjectPermissionModel.ProjectPermissionTypes.Subscriber)
+    @pagination.marshal_with(user_fields)
+    @pagination.paginate()
+    def get(self, project_id):
+        return project.get_project_users(project_id)
